772d262248337184d3cdebf3737132c033a110f6
George Markomanolis <georgios.markomanolis@kaust.edu.sa>
2018-03-01 11:35:09 +0300

    fix test number

--- stage_api_template.sh
+++ stage_api_template.sh
@@ -7,7 +7,7 @@
 #SBATCH -e bb_stage_api.err
 #DW jobdw type=scratch access_mode=striped capacity=5888GiB
 
-echo -e "Test13: Use DataWarp API "
+echo -e "Test14: Use DataWarp API "
 
 srun -n 1 stage_api $SLURM_SUBMIT_DIR/bigfile/file.txt ${DW_JOB_STRIPED}/file.txt
 
@@ -16,7 +16,7 @@ cd $DW_JOB_STRIPED
 filesize_bb=`du -hs file.txt | awk '{print $1}' | sed 's/G//'`
 if [ $filesize_pfs -eq $filesize_bb ] 
 then
- echo "13"
+ echo "14"
 fi
 
 exit 0
